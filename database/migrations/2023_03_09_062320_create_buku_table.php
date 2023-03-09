79252378236bc692ac3e1d739ef0fce4beb6eca8
Agilmhamzah <agilmhamzah4@gmail.com>
2023-03-09 13:35:54 +0700

    first commit

---
+++ database/migrations/2023_03_09_062320_create_buku_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('buku', function (Blueprint $table) {
+            $table->id();
+            $table->string('cover', 100);
+            $table->string('nama_buku', 50);
+            $table->string('penerbit', 50);
+            $table->unsignedBigInteger('jumlah_halaman');
+            $table->string('summary');
+            $table->unsignedBigInteger('qty');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('buku');
+    }
+};
